b0c9ac9ccdc475e17fdce7548d00cb3377cf3adc
Thomas <bdachai308@gmail.com>
2024-10-24 02:06:26 +0700

    more keybinds

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -11,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cascadia Code NF:size=14" };
-static const char dmenufont[]       = "Cascadia Code NF:size=14";
+static const char *fonts[]          = { "Cascadia Code:size=14" };
+static const char dmenufont[]       = "Cascadia Code:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -40,7 +41,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 
@@ -65,12 +66,17 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun" };
+static const char *powermenucmd[] = {"rofi", "-show" "power-menu" "-modi", "power-menu:~/.local/bin/rofi-power-menu", "--choices=reboot/shutdown/suspend/logout"};
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *up_vol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%",   NULL };
+static const char *down_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%",   NULL };
+static const char *mute_vol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = powermenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
@@ -83,6 +89,11 @@ static const Key keys[] = {
     { MODKEY,                       XK_l,      focusstack,   {.i = +1 } }, // right
     { MODKEY,                       XK_j,      incnmaster,   {.i = +1 } }, // down
     { MODKEY,                       XK_k,      incnmaster,   {.i = -1 } }, // up
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
+    { 0, XF86XK_AudioMute,        spawn, {.v = mute_vol } },
+    { 0, XF86XK_AudioLowerVolume, spawn, {.v = down_vol } },
+    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = up_vol } },
 	TAGKEYS(                        XK_exclam,                      0)
 	TAGKEYS(                        XK_dollar,                      1)
 	TAGKEYS(                        XK_braceleft,                      2)
