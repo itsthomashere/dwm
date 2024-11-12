7f9d8f604879b0fdfbe4cc0ee606747c03bab212
BMHDAC <bdachai308@gmail.com>
2024-11-12 22:52:25 +0700

    Config

--- config.h
+++ config.h
@@ -14,11 +14,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Cascadia Code NF:size=14" };
 static const char dmenufont[]       = "Cascadia Code NF:size=14";
-static const char col_gray1[]       = "#1d2021";
+static const char col_gray1[]       = "#141312";
 static const char col_gray2[]       = "#d3689b";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#cc241d";
-static const char col_cyan[]        = "#1d2021";
+static const char col_cyan[]        = "#141312";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
