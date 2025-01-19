0c40f628b1884098f9eef22d07ea3e2f9c236700
Thomas <bdachai308@gmail.com>
2025-01-19 23:23:31 +0700

    Add xresource

--- config.h
+++ config.h
@@ -1,27 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
+static unsigned int gappx     = 8;        /* gaps between windows */
+static unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cascadia Code NF:size=14" };
-static const char dmenufont[]       = "Cascadia Code NF:size=14";
-static const char col_gray1[]       = "#141312";
-static const char col_gray2[]       = "#d3689b";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#cc241d";
-static const char col_cyan[]        = "#141312";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
+static int showbar            = 1;        /* 0 means no bar */
+static int topbar             = 1;        /* 0 means bottom bar */
+static char font[]			= "Cascadia Code NF:size=14" ;
+static char dmenufont[]       = "Cascadia Code NF:size=14";
+static const char *fonts[]          = { font };
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -38,11 +40,11 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const int attachbelow = 1;    /* 1 means attach after the currently active window */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static int attachbelow = 1;    /* 1 means attach after the currently active window */
 
 #include "fibonacci.c"
 static const Layout layouts[] = {
@@ -82,7 +84,28 @@ static const char *play_pause[] = { "playerctl", "play-pause", NULL };
 static const char *bright_up[] = { "brightnessctl", "set", "5%+", NULL };
 static const char *bright_down[] = { "brightnessctl", "set", "5%-", NULL };
 
-#include <X11/XF86keysym.h>;
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",               STRING,  &font },
+		{ "dmenufont",          STRING,  &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "snap",          		INTEGER, &snap },
+		{ "showbar",          	INTEGER, &showbar },
+		{ "topbar",          	INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+		{ "mfact",      	 	FLOAT,   &mfact },
+};
+
+#include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -109,9 +132,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
     { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotcmd}  },
-    { 0, XF86XK_AudioMute,          spawn, {.v = mute_vol } },
-    { 0, XF86XK_AudioLowerVolume,   spawn, {.v = down_vol } },
-    { 0, XF86XK_AudioRaiseVolume,   spawn, {.v = up_vol } },
+    /* { 0, XF86XK_AudioMute,          spawn, {.v = mute_vol } }, */
+    /* { 0, XF86XK_AudioLowerVolume,   spawn, {.v = down_vol } }, */
+    /* { 0, XF86XK_AudioRaiseVolume,   spawn, {.v = up_vol } }, */
     { 0, XF86XK_AudioPlay,          spawn, {.v = play_pause } },
     { 0, XF86XK_AudioNext,          spawn, {.v = next_track } },
     { 0, XF86XK_AudioPrev,          spawn, {.v = prev_track } },
